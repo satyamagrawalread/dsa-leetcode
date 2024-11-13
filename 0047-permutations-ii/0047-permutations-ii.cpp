58e49351e59df907435922624087bc98b4438e23
Satyam Agrawal <53185604+satyamagrawalread@users.noreply.github.com>
2024-11-13 21:38:48 +0530

    Time: 2 ms (77.94%), Space: 12.1 MB (45.04%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    void solve(vector<int> &nums, int currIndex, vector<vector<int>> &result) {
-        if(currIndex==nums.size()-1) {
-            result.push_back(nums);
+    void solve(vector<int> &comb, map<int, int> &counter, int N, vector<vector<int>> &result) {
+        if(comb.size() == N) {
+            result.push_back(comb);
             return;
         }
-        int temp;
-        map<int, int> mp;
-        for(int i=currIndex; i<nums.size(); i++) {
-            if(mp.count(nums[i]) > 0) {
-                continue;
-            }
-            mp[nums[i]] = 1;
-            temp = nums[currIndex];
-            nums[currIndex] = nums[i];
-            nums[i] = temp;
-            solve(nums, currIndex+1, result);
-            temp = nums[i];
-            nums[i] = nums[currIndex];
-            nums[currIndex] = temp;
+        for (auto item : counter) {
+            int num = item.first;
+            int count = item.second;
+            if(count==0) continue;
+            comb.push_back(num);
+            counter[num]--;
+            solve(comb, counter, N, result);
+            comb.pop_back();
+            counter[num]++;
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> result;
-        solve(nums, 0, result);
+        map<int, int> counter;
+        vector<int> comb;
+        for(int num : nums) counter[num]++;
+        solve(comb, counter, nums.size(), result);
         return result;
     }
 };
