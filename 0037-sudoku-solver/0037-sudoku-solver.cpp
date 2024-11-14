85b0663876bc184ca1c27514df32adb0b5bc6aea
Satyam Agrawal <53185604+satyamagrawalread@users.noreply.github.com>
2024-11-14 13:43:03 +0530

    Time: 10 ms (80.51%), Space: 8 MB (71.91%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,63 +1,48 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>> &board, vector<vector<char>> &currBoard, int row, int col) {
-        if(row>=9 && col==0) {
-            board = currBoard;
-            return true;
+    void solve(vector<vector<char>> &board, int row, int col) {
+        if (row == 9) return;  // Completed board
+        if (board[row][col] != '.') {
+            // Move to the next cell
+            if (col < 8) solve(board, row, col + 1);
+            else solve(board, row + 1, 0);
+            return;
         }
-        bool stop = false;
-        if(board[row][col]!='.'){
-            if(col<8) {
-                stop = solve(board, currBoard, row, col+1) || false;
-            }
-            else {
-                stop = solve(board, currBoard, row+1, 0) || false;
-            }
-        }
-        else {
-            for(char c='1'; c<='9'; c++) {
-                int currGStartRow = (row/3)*3;
-                int currGStartCol = (col/3)*3;
-                bool invalid = false;
-                for(int i=currGStartRow; i<currGStartRow+3; i++) {
-                    for(int j=currGStartCol; j<currGStartCol+3; j++) {
-                        if(currBoard[i][j]!='.' && currBoard[i][j]==c) {
-                            invalid = true;
-                            break;
-                        }
-                    }
-                    if(invalid) {
-                        break;
-                    }
-                }
-                if(invalid==false) {
-                    for(int i=0; i<9; i++) {
-                        if((currBoard[row][i]!='.' && currBoard[row][i]==c) || (currBoard[i][col]!='.' && currBoard[i][col]==c)) {
-                            invalid=true;
-                            break;
-                        }
-                    }
-                }
-                if(!invalid) {
-                    currBoard[row][col] = c;
-                    if(col<8) {
-                        stop = solve(board, currBoard, row, col+1) || false;
-                    }
-                    else {
-                        stop = solve(board, currBoard, row+1, 0) || false;
-                    }
-                    if(stop) {
-                        return stop;
-                    }
-                    currBoard[row][col] = '.';
-                    
-                }
+
+        for (char c = '1'; c <= '9'; c++) {
+            if (isValid(board, row, col, c)) {
+                board[row][col] = c;
+                // Move to the next cell
+                if (col < 8) solve(board, row, col + 1);
+                else solve(board, row + 1, 0);
+
+                // Stop if the board is fully filled (solution found)
+                if (isSolved(board)) return;
+                
+                board[row][col] = '.';  // Backtrack
             }
         }
-        return stop;
+    }
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+        int gridRow = (row / 3) * 3;
+        int gridCol = (col / 3) * 3;
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c || board[i][col] == c || 
+                board[gridRow + i / 3][gridCol + i % 3] == c) return false;
+        }
+        return true;
+    }
+
+    bool isSolved(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') return false;
+            }
+        }
+        return true;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        vector<vector<char>> currBoard{board};
-        solve(board, currBoard, 0, 0);
+        solve(board, 0, 0);
     }
 };
