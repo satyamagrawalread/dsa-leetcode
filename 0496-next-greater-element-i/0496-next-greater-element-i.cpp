460f3a3f7b1b0879c149eb2960ba4679cf7785ff
Satyam Agrawal <53185604+satyamagrawalread@users.noreply.github.com>
2024-11-14 21:09:28 +0530

    Time: 0 ms (100%), Space: 12.4 MB (45.56%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,16 +4,16 @@ public:
         vector<int> result;
         unordered_map<int, int> mp;
         stack<int> st;
-        for(int i=nums2.size()-1; i>=0; i--) {
-            while(!st.empty() && st.top() < nums2[i]) {
+        for(int i=0;i<nums2.size();i++){
+            while(!st.empty() && st.top()<nums2[i]){
+                mp[st.top()]=nums2[i];
                 st.pop();
             }
-            if(st.empty()) mp[nums2[i]] = -1;
-            else mp[nums2[i]] = st.top();
             st.push(nums2[i]);
         }
         for(int i=0; i<nums1.size(); i++) {
-            result.push_back(mp[nums1[i]]);
+            if(mp.find(nums1[i])==mp.end()) result.push_back(-1);
+            else result.push_back(mp[nums1[i]]);
         }
         return result;
     }
