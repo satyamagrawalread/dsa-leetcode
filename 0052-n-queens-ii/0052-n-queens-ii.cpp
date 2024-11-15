f8d7ad67f4eedd142f2a022faed0da47dca00445
Satyam Agrawal <53185604+satyamagrawalread@users.noreply.github.com>
2024-11-15 14:42:07 +0530

    Time: 0 ms (100%), Space: 8.5 MB (40.43%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,60 +1,31 @@
 class Solution {
 public:
-    void solve(int n, int row, int col, int count, vector<string> &current, int &result) {
+    void solve(int n, int row, vector<bool> &cols, vector<bool> &mainDiag, vector<bool> &antiDiag, int &result) {
         if(row>=n) {
-            if(count==n) {
-                result++;
-            }
+            result++;
             return;
         }
-        for(int i=col; i<n; i++) {
-            if(isValid(current, row, i, n)) {
-                current[row][i] = 'Q';
-                solve(n, row+1, 0, count+1, current, result);
-                current[row][i] = '.';
+        for(int col=0; col<cols.size(); col++) {
+            int mainDiagIndex = col - row + n -1;
+            int antiDiagIndex = row + col;
+            if(!cols[col] && !mainDiag[mainDiagIndex] && !antiDiag[antiDiagIndex]) {
+                cols[col] = true;
+                mainDiag[mainDiagIndex] = true;
+                antiDiag[antiDiagIndex] = true;
+                solve(n, row+1, cols, mainDiag, antiDiag, result);
+                cols[col] = false;
+                mainDiag[mainDiagIndex] = false;
+                antiDiag[antiDiagIndex] = false;
             }
         }
     }
-    bool isValid(vector<string> & current, int row, int col, int n) {
-        for(int i=0; i<n; i++) {
-            if(current[row][i]=='Q' || current[i][col]=='Q') {
-                return false;
-            }
-        }
-        int currRow = row;
-        int currCol = col;
-        while(currRow>=0 && currCol>=0) {
-            if(current[currRow][currCol]=='Q') return false;
-            currRow--;
-            currCol--;
-        }
-        currRow = row;
-        currCol = col;
-        while(currRow<n && currCol<n) {
-            if(current[currRow][currCol]=='Q') return false;
-            currRow++;
-            currCol++;
-        }
-        currRow = row;
-        currCol = col;
-        while(currRow<n && currCol>=0) {
-            if(current[currRow][currCol]=='Q') return false;
-            currRow++;
-            currCol--;
-        }
-        currRow = row;
-        currCol = col;
-        while(currRow>=0 && currCol<n) {
-            if(current[currRow][currCol]=='Q') return false;
-            currRow--;
-            currCol++;
-        }
-        return true;
-    }
+    
     int totalNQueens(int n) {
         int result = 0;
-        vector<string> current(n, string(n, '.'));
-        solve(n, 0, 0, 0, current, result);
+        vector<bool> cols(n, false);
+        vector<bool> mainDiag(2*n-1, false);
+        vector<bool> antiDiag(2*n-1, false);
+        solve(n, 0, cols, mainDiag, antiDiag, result);
         return result;
     }
 };
